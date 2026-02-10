610a4783815cf9bf1aefe71171f3041287289953
equal-matirx9 <nafeesnassar123@gmail.com>
2026-02-10 09:47:02 +0530

    Implemented UC1: Printing OOPS to console with JavaDocs

---
+++ OOPSBannerApp.java
@@ -0,0 +1,13 @@
+/**
+ * OOPS Banner App - Use Case 1
+ * This class serves as the entry point for the application.
+ * * @author Nafees
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        // UC1: Print the literal text "OOPS" to the console
+        System.out.println("OOPS");
+    }
+}
