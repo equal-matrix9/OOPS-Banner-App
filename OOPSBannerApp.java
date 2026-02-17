ba250db6daf2e63bef18aac84527f0c11c1d53b3
equal-matirx9 <nafeesnassar123@gmail.com>
2026-02-17 08:53:27 +0530

    Completed UC2: Implemented ASCII OOPS banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,25 @@
 /**
- * OOPS Banner App - Use Case 1
- * This class serves as the entry point for the application.
- * * @author Nafees
- * @version 1.0
+ * OOPSBannerApp UC2 - Banner Display Application
+ *
+ * This class demonstrates the creation of a visual banner displaying the word "OOPS"
+ * using ASCII art composed of asterisks (*) and spaces. The banner is printed to the
+ * console using individual print statements for each line.
+ *
+ * @author Developer
+ * @version 2.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        // UC1: Print the literal text "OOPS" to the console
-        System.out.println("OOPS");
+        // UC2: Print "OOPS" using specific string concatenation to match the reference image
+        System.out.println("    ***    " + "    ***    " + " *******    "   + "    ***** ");
+        System.out.println("  **   **  " + "  **   **  " + " **     **  "   + "  **      ");
+        System.out.println(" **     ** " + " **     ** " + " **      ** "   + "**        ");
+        System.out.println(" **     ** " + " **     ** " + " **     **  "   + " **       ");
+        System.out.println(" **     ** " + " **     ** " + " *******    "   + "   ***    ");
+        System.out.println(" **     ** " + " **     ** " + " **         "   + "      **  ");
+        System.out.println(" **     ** " + " **     ** " + " **         "   + "       ** ");
+        System.out.println("  **   **  " + "  **   **  " + " **         "   + "     **   ");
+        System.out.println("    ***    " + "    ***    " + " **         "   + "*****     ");
     }
 }
